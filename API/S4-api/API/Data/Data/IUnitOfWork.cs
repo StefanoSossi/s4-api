31fb0fb79ee3f716458197ac2e9bd763c06266ad
Stefano Sossi <stefano.sossi@truextend.com>
2024-07-04 11:06:54 -0400

    [feature/s4-3]: Added unit of work

---
+++ API/S4-api/API/Data/Data/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace s4.Data
+{
+    public interface IUnitOfWork
+    {
+        void BeginTransaction();
+        void CommitTransaction();
+        void RollBackTransaction();
+        void Save();
+    }
+}
---
+++ API/S4-api/API/Data/Data/UnitOfWork.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using s4.Data.Repository.Generic;
+using Serilog;
+
+namespace s4.Data
+{
+    public class UnitOfWork
+    {
+        private readonly S4DBContext _S4DBContext;
+
+        public void BeginTransaction()
+        {
+            _S4DBContext.Database.BeginTransaction();
+        }
+
+        public void CommitTransaction()
+        {
+            _S4DBContext.Database.CommitTransaction();
+        }
+
+        public void RollBackTransaction()
+        {
+            _S4DBContext.Database.RollbackTransaction();
+        }
+
+        public void Save()
+        {
+            try
+            {
+                BeginTransaction();
+                _S4DBContext.SaveChanges();
+                CommitTransaction();
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                RollBackTransaction();
+                string message = $"Error to save changes on Database -> Save() {Environment.NewLine}Message: {ex.Message}{Environment.NewLine}";
+                Log.Error(ex, $"{message}{Environment.NewLine} Stack trace: {Environment.NewLine}");
+                throw new DatabaseException("Can not save changes, error in Database", ex.InnerException);
+            }
+            catch (DbUpdateException ex)
+            {
+                RollBackTransaction();
+                string message = $"Error to save changes on Database -> Save() {Environment.NewLine}Message: {ex.Message}{Environment.NewLine}";
+                Log.Error(ex, $"{message}{Environment.NewLine} Stack trace: {Environment.NewLine}");
+                throw new DatabaseException("Can not save changes, error in Database", ex.InnerException);
+            }
+            catch (Exception ex)
+            {
+                string message = $"Error to save changes on Database -> Save() {Environment.NewLine}Message: {ex.Message}{Environment.NewLine}";
+                Log.Error(ex, $"{message}{Environment.NewLine} Stack trace: {Environment.NewLine}");
+                throw new DatabaseException("Can not save changes, error in Database", ex.InnerException);
+            }
+        }
+    }
+}
