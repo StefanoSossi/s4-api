efd9b899095935268f1b0d18b4580e2a2f2ac326
Stefano Sossi <stefano.sossi@truextend.com>
2024-07-04 10:20:11 -0400

    [feature/s4-2]: Added dto models and validation

---
+++ API/S4-api/API/Logic/Logic/Models/StudentDto.cs
@@ -0,0 +1,15 @@
+﻿using s4.Data.Models;
+
+namespace s4.Logic.Models
+{
+    public class StudentDto
+    {
+        public Guid Id { get; set; }
+        public string? LastName { get; set; }
+        public required string FirstName { get; set; }
+
+        public ICollection<StudentClass>? StudentClasses { get; set; }
+
+        
+    }
+}
