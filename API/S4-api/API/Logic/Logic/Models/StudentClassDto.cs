48d8498713bcde9063fb7e3e1ce8ad8553d745c7
Stefano Sossi <stefano.sossi@truextend.com>
2024-07-04 13:53:59 -0400

    [feature/s4-2]: Added mapper

---
+++ API/S4-api/API/Logic/Logic/Models/StudentClassDto.cs
@@ -0,0 +1,17 @@
+﻿using s4.Data.Models;
+using s4.Logic.Models.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace s4.Logic.Models
+{
+    public class StudentClassDto
+    {
+        public Guid Id { get; set; }
+        public Guid StudentId { get; set; }
+        public Guid ClassId { get; set; }
+    }
+}
